0e9fe0d590583866b45aa8719a89b4ac8451f1c1
himadri shekhar dandapat <himadri.carrer@live.com>
2023-03-22 22:00:41 +0100

    tsconfig fix

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-export * from './sharedData';
-export * from './standalone';
+export interface Coach {
+    userId: number;
+    userUUID: string;
+}
+
+export interface Patient {
+    patientId: number;
+    patientUuid: string;
+}
+
+export interface ParentMethods {
+    showToast: (text: string) => void;
+    onNoteCreated: () => void;
+}
+
+export interface SharedData {
+    patient: Patient;
+    coach: Coach;
+    SharedData: SharedData;
+}
